3ce940946a4ae154baee36c8dadd3eaebd092b19
Buomkuoth Yian <buom.nyajal@gmail.com>
2024-09-25 20:05:58 +0300

    Finally solved the bug of not scheduling before cancelling

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://eb62ff33fb12422f0f2291341d0d116e@o4508014381694976.ingest.us.sentry.io/4508014389428224",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
